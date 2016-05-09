d5ff5d83533aa406817b339a0e698d982c50279b
soulFreedom <844360369@qq.com>
2016-05-09 23:18:07 +0800

    test

--- src/main/java/com/investdata/action/LoginAction.java
+++ src/main/java/com/investdata/action/LoginAction.java
@@ -5,6 +5,9 @@ public class LoginAction {
 	
 	public static void main(String[] args) {
 		System.out.println();
+		int times = 0;
+		List<String> aa = new ArrayList<String>();
+		
 		//lizhibin fjfjfjfjfj okkkk
 	}
 }
