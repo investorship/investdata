cff581b285b74caf732634cdded1f05fb11d043d
soulFreedom <844360369@qq.com>
2016-05-09 23:07:06 +0800

    sfd

--- src/main/java/com/investdata/action/LoginAction.java
+++ src/main/java/com/investdata/action/LoginAction.java
@@ -2,10 +2,7 @@ package com.investdata.action;
 
 public class LoginAction {
 	
-	//test for ghl
-	//test2
-	//aa
-	///
+	
 	public static void main(String[] args) {
 		System.out.println();
 		//lizhibin fjfjfjfjfj
