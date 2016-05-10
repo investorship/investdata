0b6e37c35f85499871eb7a7ae1da256772023005
soulFreedom <844360369@qq.com>
2016-05-10 17:26:59 +0800

    提交

---
+++ src/main/java/com/investdata/common/BaseAction.java
@@ -0,0 +1,8 @@
+package com.investdata.common;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BaseAction extends ActionSupport {
+	public static final String SUCCESS = "success";
+	public static final String FAIL = "fail";
+}
