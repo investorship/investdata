bce4a999b4e79b8047ce5142aab6ffe22818cdaf
soulFreedom <844360369@qq.com>
2016-05-11 16:36:48 +0800

    rename dao

--- src/main/java/com/investdata/dao/TUserDao.java
+++ src/main/java/com/investdata/dao/TUserDao.java
@@ -3,7 +3,7 @@ package com.investdata.dao;
 import com.investdata.dao.po.User;
 
 
-public interface IUserDao {
+public interface TUserDao {
 	public void add(User user) throws Exception;
 	public User getByPk(User user) throws Exception;
 	
--- src/main/java/com/investdata/dao/impl/UserDaoImpl.java
+++ src/main/java/com/investdata/dao/impl/UserDaoImpl.java
@@ -1,9 +1,9 @@
 package com.investdata.dao.impl;
 
-import com.investdata.dao.IUserDao;
+import com.investdata.dao.TUserDao;
 import com.investdata.dao.po.User;
 
-public class UserDaoImpl extends BaseDao implements IUserDao {
+public class UserDaoImpl extends BaseDao implements TUserDao {
 
 	public void add(User user) throws Exception {
 		dao.insert("",user);
