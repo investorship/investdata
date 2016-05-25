94be590c0cb25e147ec75b03c8350dd5dae9509e
soulFreedom <844360369@qq.com>
2016-05-25 13:23:25 +0800

    修改t_user表字段默认值

--- sql/investdata.sql
+++ sql/investdata.sql
@@ -13,11 +13,11 @@ create table t_user
    username             char(30) not null comment '用户名',
    password             char(50) not null comment '密码',
    email                char(30) not null comment '邮箱',
-   ispayer				int not null comment '是否付费用户 0-否 1-是',
-   paydate				timestamp comment '付费时间',
-   enddate				timestamp comment '截止时间',
-   flag                 int not null comment '标志位 0-停用 1-启用',
-   intime               timestamp not null comment '入库时间',
+   ispayer				int not null default 0 comment '是否付费用户 0-否 1-是',
+   paydate				char(10) default ''  comment '付费时间',
+   enddate				char(10) default '' comment '截止时间',
+   flag                 int not null default 0 comment '标志位 0-停用 1-启用',
+   intime               timestamp not null DEFAULT now() comment '入库时间',
    primary key (username)
 );
 
