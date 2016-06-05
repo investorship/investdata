8e98b8f34bc2d09df740f04f3a1314b3242a6608
soulFreedom <844360369@qq.com>
2016-06-05 16:29:15 +0800

    Create BaseAction.java

---
+++ src/main/java/com/investdata/common/BaseAction.java
@@ -0,0 +1,17 @@
+package com.investdata.common;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BaseAction extends ActionSupport {
+	public static final String SUCCESS = "success";
+	public static final String INPUT = "input";
+	public static final String FAIL = "fail";
+	public static final String LOGIN_SUCC = "login_succ";
+	public static final String LOGIN_FAIL = "login_fail";
+	public static final String LOGOUT = "logout";
+	public static final String REG_SUCC = "reg_succ";
+	public static final String AJAX = "ajax";
+	public static final String AJAX_ADMIN = "ajax_admin";
+	public static final String ACTIVE_FAIL = "active_fail";
+	public static final String ACTIVE_SUCC = "active_succ";
+}
