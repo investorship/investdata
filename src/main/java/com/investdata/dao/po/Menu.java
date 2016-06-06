8b0f7ba2cd0e3480f7c1e8e8aa1b188414f1cd30
Bill <developerli@126.com>
2016-06-06 14:53:39 +0800

    Change-Id: Ibfcd4b9212d0cc4ea51edd5e7c918e5b5b07737b

---
+++ src/main/java/com/investdata/dao/po/Menu.java
@@ -0,0 +1,63 @@
+package com.investdata.dao.po;
+
+import java.sql.Timestamp;
+
+/**
+ * 后台管理菜单树
+ * @author Bill
+ *
+ */
+public class Menu {
+	
+		private int id;//菜单id
+		private int pid;//菜单父id
+		private String name;//菜单名称
+		private int isleaf;//是否叶子节点，0-否，1-是
+		private int flag;//是否启用标记,0-禁用，1-启用
+		private Timestamp intime;//入库时间
+		private String url;//url
+		
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public int getPid() {
+			return pid;
+		}
+		public void setPid(int pid) {
+			this.pid = pid;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getIsleaf() {
+			return isleaf;
+		}
+		public void setIsleaf(int isleaf) {
+			this.isleaf = isleaf;
+		}
+		public int getFlag() {
+			return flag;
+		}
+		public void setFlag(int flag) {
+			this.flag = flag;
+		}
+		public Timestamp getIntime() {
+			return intime;
+		}
+		public void setIntime(Timestamp intime) {
+			this.intime = intime;
+		}
+		public String getUrl() {
+			return url;
+		}
+		public void setUrl(String url) {
+			this.url = url;
+		}
+
+}
